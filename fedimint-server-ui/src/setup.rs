eb4035f6aa0be99cdc97311a7a3594aea421fd70
m1sterc001guy <jumoell@protonmail.com>
2026-01-19 16:46:22 -0600

    chore: use UMD scanner

--- fedimint-server-ui/src/setup.rs
+++ fedimint-server-ui/src/setup.rs
@@ -66,7 +66,7 @@ pub fn setup_layout(title: &str, content: Markup) -> Markup {
                     }
                 }
                 script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" {}
-                script type="module" src="/assets/qr-scanner.min.js" {}
+                script src="/assets/qr-scanner.umd.min.js" {}
             }
         }
     }
